62ed7484b7ab3ae7e421040fb5e41c27d1db937c
yamamoto-jks <yamamoto-daiki@jpn-ks.co.jp>
2026-05-27 11:29:23 +0900

    [infra]フロントエンドのCDK化（S3, CloudFront）

--- infra/bin/server.ts
+++ infra/bin/server.ts
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { ServerStack } from '../lib/server-stack';
+import { ClientStack } from '../lib/client-stack';
+import { Construct } from 'constructs';
 
 const app = new cdk.App();
-new ServerStack(app, 'ServerStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const stackParam = (id: string): [Construct, string, cdk.StackProps] => [
+  app,
+  id,
+  {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  },
+];
+
+new ServerStack(...stackParam('ServerStack'));
+new ClientStack(...stackParam('ClientStack'));
---
+++ infra/lib/client-stack.ts
@@ -0,0 +1,63 @@
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import {
+  CachedMethods,
+  Distribution,
+  ErrorResponse,
+  ResponseHeadersPolicy,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import path from 'path';
+
+export class ClientStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const websiteBucket = new Bucket(this, 'MetaFormWebsiteBucket', {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const distribution = new Distribution(this, 'MetaUiDistribution', {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachedMethods: CachedMethods.CACHE_GET_HEAD,
+        responseHeadersPolicy: new ResponseHeadersPolicy(this, 'CustomResponseHeadersPolicy', {
+          corsBehavior: {
+            accessControlAllowOrigins: ['*'],
+            accessControlAllowMethods: ['GET', 'HEAD', 'OPTIONS'],
+            accessControlAllowHeaders: ['*'],
+            accessControlAllowCredentials: false,
+            originOverride: true,
+          },
+        }),
+      },
+      errorResponses: [403, 404].map(
+        (errorStatusCode): ErrorResponse => ({
+          httpStatus: errorStatusCode,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: Duration.seconds(0),
+        }),
+      ),
+      defaultRootObject: 'index.html',
+    });
+
+    new BucketDeployment(this, 'DeployMetaUi', {
+      sources: [Source.asset(path.join(__dirname, '../../dist/meta_ui/browser'))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    new CfnOutput(this, 'MetaUiAppUrl', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'The URL of the Angular Application',
+    });
+  }
+}
